d04e44c236a7cea0d47f2e10d63da1a85a5ca4c2
Miradin <alexandr0202@gmail.com>
2018-10-24 01:39:55 +0200

    homework 1

---
+++ SpamTools/SpamLib/Info.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace SpamLib
+{
+    static class Info
+    {
+        public static string GetServerName() { return "smtp.yandex.ru"; }
+        public static string GetServerPort() { return "25"; }
+    }
+}
